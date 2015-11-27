844e6683bf34312bec254e8bf741c16fe3d838b5
sunwei <lx_sunwei@163.comm>
2015-11-27 11:08:19 +0800

    crashHandle

---
+++ app/src/main/java/com/materialdesigndemo/module/MainApplication.java
@@ -0,0 +1,21 @@
+package com.materialdesigndemo.module;
+
+import android.app.Application;
+
+import com.materialdesigndemo.utils.CrashHandler;
+
+/**
+ * Created by sunwei on 2015/11/27.
+ * Email: lx_sunwei@163.com.
+ * Description:
+ */
+public class MainApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        CrashHandler crashHandler = CrashHandler.getInstance();
+        crashHandler.init(getApplicationContext());
+    }
+}
---
+++ app/src/main/java/com/materialdesigndemo/utils/CrashHandler.java
@@ -0,0 +1,227 @@
+package com.materialdesigndemo.utils;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.lang.Thread.UncaughtExceptionHandler;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.os.Looper;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+/**
+ * UncaughtException处理类,当程序发生Uncaught异常的时候,有该类 来接管程序,并记录 发送错误报告.
+ * 
+ */
+public class CrashHandler implements UncaughtExceptionHandler {
+	/** Debug Log tag */
+	public static final String TAG = "CrashHandler";
+	/**
+	 * 是否开启日志输出,在Debug状态下开启, 在Release状态下关闭以提示程序性能
+	 * */
+	public static final boolean DEBUG = true;
+	/** 系统默认的UncaughtException处理类 */
+	private UncaughtExceptionHandler mDefaultHandler;
+	/** CrashHandler实例 */
+	private static CrashHandler INSTANCE;
+	/** 程序的Context对象 */
+	private Context mContext;
+
+	/** 使用Properties来保存设备的信息和错误堆栈信息 */
+	private Map<String, Object> mDeviceCrashInfo = new HashMap<String, Object>();
+	private static final String VERSION_NAME = "versionName";
+	private static final String VERSION_CODE = "versionCode";
+	private static final String STACK_TRACE = "STACK_TRACE";
+	/** 错误报告文件的扩展名 */
+	private static final String CRASH_REPORTER_EXTENSION = ".cr";
+
+	/** 保证只有一个CrashHandler实例 */
+	private CrashHandler() {
+	}
+
+	/** 获取CrashHandler实例 ,单例模式 */
+	public static CrashHandler getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new CrashHandler();
+		}
+		return INSTANCE;
+	}
+
+	/**
+	 * 初始化,注册Context对象, 获取系统默认的UncaughtException处理器, 设置该CrashHandler为程序的默认处理器
+	 * 
+	 * @param ctx
+	 */
+	public void init(Context ctx) {
+		mContext = ctx;
+		mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler();
+		Thread.setDefaultUncaughtExceptionHandler(this);
+	}
+
+	/**
+	 * 当UncaughtException发生时会转入该函数来处理
+	 */
+
+	public void uncaughtException(Thread thread, Throwable ex) {
+		if (!handleException(ex) && mDefaultHandler != null) {
+			// 如果用户没有处理则让系统默认的异常处理器来处理
+			mDefaultHandler.uncaughtException(thread, ex);
+		} else {
+			// Sleep一会后结束程序
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				Log.e(TAG, "Error : ", e);
+			}
+			android.os.Process.killProcess(android.os.Process.myPid());
+			System.exit(10);
+		}
+	}
+
+	/**
+	 * 自定义错误处理,收集错误信息 发送错误报告等操作均在此完成. 开发者可以根据自己的情况来自定义异常处理逻辑
+	 * 
+	 * @param ex
+	 * @return true:如果处理了该异常信息;否则返回false
+	 */
+	private boolean handleException(Throwable ex) {
+		if (ex == null) {
+			return true;
+		}
+		 ex.getLocalizedMessage();
+		// 使用Toast来显示异常信息
+		new Thread() {
+			@Override
+			public void run() {
+				Looper.prepare();
+//				Toast.makeText(mContext, "Exception!", Toast.LENGTH_LONG)
+//						.show();
+				Looper.loop();
+			}
+
+		}.start();
+		// 收集设备信息
+		collectCrashDeviceInfo(mContext);
+		// 保存错误报告文件
+		saveCrashInfoToFile(ex);
+		// 发送错误报告到服务器
+		return true;
+	}
+
+	/**
+	 * 保存错误信息到文件中
+	 * 
+	 * @param ex
+	 * @return
+	 */
+	private String saveCrashInfoToFile(Throwable ex) {
+		Writer info = new StringWriter();
+		PrintWriter printWriter = new PrintWriter(info);
+		ex.printStackTrace(printWriter);
+		ex.printStackTrace();
+		Throwable cause = ex.getCause();
+		while (cause != null) {
+			cause.printStackTrace(printWriter);
+			cause = cause.getCause();
+		}
+
+		String result = info.toString();
+		printWriter.close();
+		mDeviceCrashInfo.put(STACK_TRACE, result);
+
+		try {
+			
+			long timestamp = System.currentTimeMillis();
+			String fileName = "crash-" + timestamp + CRASH_REPORTER_EXTENSION;
+			File fileDir = mContext.getExternalCacheDir();
+			if (!fileDir.exists()) {
+				fileDir.mkdir();
+			}
+			File fileCache = new File(fileDir, fileName);
+			FileOutputStream trace = new FileOutputStream(fileCache);
+			/*FileOutputStream trace = mContext.openFileOutput(fileName,
+					Context.MODE_PRIVATE);*/
+			Iterator<String> it = mDeviceCrashInfo.keySet().iterator();
+			while (it.hasNext()) {
+				String key = it.next();
+				Object value = mDeviceCrashInfo.get(key);
+				trace.write((key.toString() + "=" + value.toString() + "\n")
+						.getBytes());
+				
+			}
+			trace.flush();
+			trace.close();
+			return fileName;
+		} catch (Exception e) {
+			Log.e(TAG, "an error occured while writing report file...", e);
+		}
+		return null;
+	}
+
+	/**
+	 * 收集程序崩溃的设备信息
+	 * 
+	 * @param ctx
+	 */
+	public void collectCrashDeviceInfo(Context ctx) {
+		try {
+			PackageManager pm = ctx.getPackageManager();
+			PackageInfo pi = pm.getPackageInfo(ctx.getPackageName(), 0);
+			if (pi != null) {
+				mDeviceCrashInfo.put(VERSION_NAME,
+						pi.versionName == null ? "not set" : pi.versionName);
+				mDeviceCrashInfo.put(VERSION_CODE, pi.versionCode);
+			}
+			TelephonyManager tm = (TelephonyManager) ctx
+					.getSystemService(Context.TELEPHONY_SERVICE);
+			mDeviceCrashInfo.put("PHONE_NUMBER", tm.getLine1Number());
+
+			WifiManager wifi = (WifiManager) ctx
+					.getSystemService(Context.WIFI_SERVICE);
+			WifiInfo wifiInfo = wifi.getConnectionInfo();
+			mDeviceCrashInfo.put("mac", wifiInfo.getMacAddress());
+
+		} catch (NameNotFoundException e) {
+			Log.e(TAG, "Error", e);
+		} catch (Exception e) {
+			Log.e(TAG, "Error", e);
+		}
+		// 使用反射来收集设备信息.在Build类中包含各种设备信息,
+		// 例如: 系统版本号,设备生产商 等帮助调试程序的有用信息
+		// 具体信息请参考后面的截图
+		Field[] fields = Build.class.getDeclaredFields();
+		for (Field field : fields) {
+			try {
+				field.setAccessible(true);
+				mDeviceCrashInfo.put(field.getName(), field.get(null));
+				if (DEBUG) {
+					Log.d(TAG, field.getName() + " : " + field.get(null));
+				}
+			} catch (Exception e) {
+				Log.e(TAG, "Error while collect crash info", e);
+			}
+
+		}
+
+	}
+}
