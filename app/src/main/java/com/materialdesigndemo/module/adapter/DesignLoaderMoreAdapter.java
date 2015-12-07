80fa9d76c536c65991db35646bdb6eb045ab3cea
sunwei <lx_sunwei@163.comm>
2015-12-07 16:31:16 +0800

    conflict

--- app/src/main/java/com/materialdesigndemo/module/adapter/DesignLoaderMoreAdapter.java
+++ app/src/main/java/com/materialdesigndemo/module/adapter/DesignLoaderMoreAdapter.java
@@ -1,10 +1,12 @@
 package com.materialdesigndemo.module.adapter;
 
 import android.support.v4.util.CircularArray;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.materialdesigndemo.R;
@@ -28,9 +30,11 @@ public class DesignLoaderMoreAdapter extends BaseLoadingAdapter<DesignItem> {
     //正常条目
     public  class DesignViewHolder extends RecyclerView.ViewHolder {
         public TextView  textView;
+        public CardView cardView;
         public DesignViewHolder(View view) {
             super(view);
             textView = (TextView) view.findViewById(R.id.tv_design);
+            cardView = (CardView) view.findViewById(R.id.cardView_designer);
 
         }
     }
@@ -46,6 +50,13 @@ public class DesignLoaderMoreAdapter extends BaseLoadingAdapter<DesignItem> {
     public void onBindNormalViewHolder(RecyclerView.ViewHolder holder, int position) {
         DesignViewHolder viewHolder = (DesignViewHolder)holder;
         DesignItem designItem = mDesignItems.get(position);
+        if (position == 10) {
+            //设置瀑布流的条目大小
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(260, 360);
+            lp.setMargins(10, 40, 10, 80);
+            viewHolder.cardView.setLayoutParams(lp);
+        }
+
         viewHolder.textView.setText(designItem.name);
     }
 }
